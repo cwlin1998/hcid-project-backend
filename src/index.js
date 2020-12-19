20dd0a5e26087802ff890eed167aa9d15f4eda71
Lin Cheng Wei <snoopy90338@gmail.com>
2020-12-19 15:01:08 +0800

    remove location api

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use('/api-docs', swagger_ui.serve, swagger_ui.setup(swagger_document))
 app.use('/plans', require('./backend/api/plan'))
-app.use('/locations', require('./backend/api/location'))
 app.use('/users', require('./backend/api/user'))
 
 server = app.listen(port, () => console.log('Listening on port ' + port))
